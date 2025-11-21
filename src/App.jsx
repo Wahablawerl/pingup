4db791cc545c15b448407edeecd911ac3dc4f3bc
Abdulwahab <abdulwahablawal2002@gmail.com>
2025-11-21 10:44:38 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { Route, Routes } from "react-router-dom";
+import Login from "./pages/Login";
+import Feed from "./pages/Feed";
+import Messages from "./pages/Messages";
+import ChatBox from "./pages/ChatBox";
+import Connection from "./pages/Connection";
+import Discover from "./pages/Discover";
+import Profile from "./pages/Profile";
+import CreatePost from "./pages/CreatePost";
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Login />}>
+          <Route path="index" element={<Feed />} />
+          <Route path="messages" element={<Messages />} />
+          <Route path="messages/:userId" element={<ChatBox />} />
+          <Route path="connections" element={<Connection />} />
+          <Route path="discover" element={<Discover />} />
+          <Route path="profile" element={<Profile />} />
+          <Route path="profile/:profileID" element={<Profile />} />
+          <Route path="create-post" element={<CreatePost />} />
+        </Route>
+      </Routes>
+    </>
+  );
+}
+
+export default App;
